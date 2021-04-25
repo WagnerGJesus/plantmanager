ceddff0535c3f3bebd1193c33c6fcb780d0ede73
WagnerGJesus <wagnergoncalvesdejesus88@gmail.com>
2021-04-25 12:58:45 -0300

    add page UserIdentification

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 
 import { Welcome } from './src/pages/Welcome';
+import { UserIdentification } from './src/pages/UserIdentification';
 import {
   useFonts,
   Jost_400Regular,
@@ -20,7 +21,8 @@ export default function App() {
   return (
     <>
     <StatusBar/>
-    <Welcome/>
+    {/* <Welcome/> */}
+    <UserIdentification/>
     </>
     );
 }
